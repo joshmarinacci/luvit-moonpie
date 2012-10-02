ebb5023bf03fd3312423c4f9ba9b12955d56ebb0
Josh Marinacci <joshua@marinacci.org>
2012-10-01 19:32:27 -0700

    make buttons fire an action event

--- experiments/rpi/scenegraph/test_button_01.lua
+++ experiments/rpi/scenegraph/test_button_01.lua
@@ -2,27 +2,33 @@ jit.off()
 
 package.path = package.path .. ";../?.lua"
 local pi = require("moonpie")
-local EB = require("eventbus").getShared()
-EB:on("action",function()
-    print("an action happened. engaged?")
-end)
-
 local scene = require("Scene")
+
+-- all of the inits
 scene.window = pi.createFullscreenWindow()
 print('window = ', scene.window)
 scene:init()
-
 require ("RectNode")
 RectNode.loadShader()
 require ("TextNode")
 TextNode.loadShader()
 require("ButtonNode")
 
+-- create a button
 local button = ButtonNode:new{
     x = 0,
     y = 0,
     text = "Engage!"
 }
 
+-- add it to the scene
 scene:add(button)
+
+-- add an event handler
+local EB = require("eventbus").getShared()
+EB:on("action",function()
+    print("an action happened. engaged = ", button.selected)
+end)
+
+
 scene.loop()
